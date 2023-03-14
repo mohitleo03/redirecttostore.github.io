3e4875c1e73f8d6355dafe2190d0560dcd5d3ea9
Pie Infosystems <pieinfosystems@Pies-MacBook-Pro.local>
2023-03-14 17:05:02 +0530

    first commit

---
+++ assets/javascript/index.js
@@ -0,0 +1,11 @@
+function openPlayStore(){
+    console.log("mohit");
+    location.href="https://play.google.com/store/apps/details?id=com.pie.yourbooks";
+}
+function openAppStore(){
+    location.href="https://apps.apple.com/us/app/yourbooks-gst-accounting/id6443643248";
+}
+function init(){
+    console.log(window.navigator.userAgent);
+}
+init();
