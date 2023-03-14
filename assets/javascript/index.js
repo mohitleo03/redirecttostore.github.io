a8c171c5ea6df32dfb808aa9760184a1e7b05677
Pie Infosystems <pieinfosystems@Pies-MacBook-Pro.local>
2023-03-14 18:40:37 +0530

    update

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -6,6 +6,20 @@ function openAppStore(){
     location.href="https://apps.apple.com/us/app/yourbooks-gst-accounting/id6443643248";
 }
 function init(){
+    //for android - Android
+    //for iphone - iPhone
+    //for ipad - iPad
+    //for mac - Mac
+    //else show html
+    let userAgent = window.navigator.userAgent;
+    if(userAgent.includes('Android')){
+        openPlayStore();
+    }
+    else if(userAgent.includes('iPhone')||userAgent.includes('iPad')||userAgent.includes('Mac')){
+        openAppStore();
+    }else{
+        
+    }
     console.log(window.navigator.userAgent);
     document.getElementById('mydiv').innerHTML = window.navigator.userAgent
 }
